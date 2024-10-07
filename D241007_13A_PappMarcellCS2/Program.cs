620beff837365eb5da017fbf34d0c35eb3ca86be
Papp.Marcell <Papp.Marcell@28-DIAK10>
2024-10-07 12:40:32 +0200

    Add project files.

---
+++ D241007_13A_PappMarcellCS2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using D241007_13A_PappMarcellCS2;
+
+List<beolv> beolvasas = [];
+using StreamReader sr = new(@"..\..\..\src\forras.txt");
+while (!sr.EndOfStream) beolvasas.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"ennyi resztvevo volt:{beolvasas.Count}");
+
+//var atlageletkor = beolvasas.Average( atlageletkor => atlageletkor.szulev);
+//Console.WriteLine($"A férfi versenyzők átlag életkora:{atlageletkor/94}");
+int i = 0;
+foreach (var asd in beolvasas) {
+    if (asd.neme == "f") 
+    {
+        i = 2024-asd.szulev;
+    }
+}
+var elitj = beolvasas.Count(p =>p.kategoria == "elit junior");
+Console.WriteLine($"versenyzök száma 'elit junior' kategoriában:{elitj}");
+Console.WriteLine($"A férfiak átlag életkora:{i}");
+int id2151 = 0;
+foreach(var ido2 in beolvasas) 
+{
+
+}
---
+++ D241007_13A_PappMarcellCS2/beolv.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D241007_13A_PappMarcellCS2
+{
+    internal class beolv
+    {
+        public beolv(string nev, int szulev, int rajtszam, string neme, string kategoria)
+        {
+            this.nev = nev;
+            this.szulev = szulev;
+            this.rajtszam = rajtszam;
+            this.neme = neme;
+            this.kategoria = kategoria;
+        }
+
+        public string nev { get; set;}
+        public int szulev { get; set;}
+        public int rajtszam { get; set;}
+        public string neme { get; set;}
+        public string kategoria { get; set;}
+        public DateTime ido { get; set;}
+        public TimeSpan depo { get; set;}
+        public TimeSpan kerekpar { get; set;}
+        public TimeSpan masokdikdepo { get; set;}
+        public TimeSpan futas { get; set; }
+        public beolv(string beolvasottSor)
+        {
+            string[] darabok = beolvasottSor.Split(';');
+            nev = darabok[0];
+            szulev = Convert.ToInt16(darabok[1]);
+            rajtszam = Convert.ToInt16(darabok[2]);
+            neme = darabok[3];
+         //   ido=DateTime.Parse(darabok[4]);
+            kategoria=darabok[4];
+
+        }
+    }
+
+}
